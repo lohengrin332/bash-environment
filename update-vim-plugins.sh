7219094c1f60e1e4a0e09d29666e019fb5d16292
JD Lewis <jlewis@indeed.com>
2017-07-06 11:43:54 -0500

    Updating plugins.

--- update-vim-plugins.sh
+++ update-vim-plugins.sh
@@ -8,8 +8,9 @@
     git subtree pull --squash --prefix=.vim/bundle/perlmodop             https://github.com/lohengrin332/perlmodop.git       master 
     git subtree pull --squash --prefix=.vim/bundle/taglist.vim           https://github.com/vim-scripts/taglist.vim.git      master 
     git subtree pull --squash --prefix=.vim/bundle/vim-perl              https://github.com/vim-perl/vim-perl.git            master 
-    git subtree pull --squash --prefix=.vim/bundle/vim-matchit           https://github.com/edsono/vim-matchit.git           master 
+    # git subtree pull --squash --prefix=.vim/bundle/vim-matchit           https://github.com/edsono/vim-matchit.git           master 
     git subtree pull --squash --prefix=.vim/bundle/mojo.vim              https://github.com/yko/mojo.vim.git                 master 
     git subtree pull --squash --prefix=.vim/bundle/typescript-vim        https://github.com/leafgarland/typescript-vim.git   master
     git subtree pull --squash --prefix=.vim/bundle/syntastic             https://github.com/vim-syntastic/syntastic.git      master
     git subtree pull --squash --prefix=.vim/bundle/vim-sleuth            https://github.com/tpope/vim-sleuth.git             master
+    git subtree pull --squash --prefix=.vim/bundle/nerdtree              https://github.com/scrooloose/nerdtree.git          master
