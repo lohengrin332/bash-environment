3a27512eda96345be5337af6d13074e41b1dd93f
JD Lewis <jlewis@indeed.com>
2024-09-13 10:57:49 -0500

    NOBUG: fixes to ssh agent process.

--- bin/get_ssh_sock.pl
+++ bin/get_ssh_sock.pl
@@ -3,17 +3,24 @@
 use strict;
 use Data::Dumper;
 
+&isAgentRunning() || &startAgent();
+
 my $current_auth_sock_file = $ENV{SSH_AUTH_SOCK};
 my $agent_file = (-e $current_auth_sock_file) ? $current_auth_sock_file : &getSSHSock();
 
 print "$agent_file";
 
 
+sub isAgentRunning {
+  my $agent_pid = `/usr/bin/pidof ssh-agent`;
+  return $agent_pid ? 1 : 0;
+}
+
+sub startAgent {
+  `/usr/bin/ssh-agent`;
+}
 
 sub getSSHSock {
-  my $pid = shift;
-  $pid = $pid ? $pid : $$;
-  #print STDERR "getSSHSock ($pid)\n";
   my $user = getpwuid($<);
   my $find_command = "/usr/bin/find /tmp/ -maxdepth 1 -type d -name ssh-\\* -user $user";
   my @dirs = `$find_command`;
@@ -26,7 +33,9 @@ sub getSSHSock {
       chomp(@ls);
       $first_ssh_agent_file = $ls[0];
     }
+    last if(-e $first_ssh_agent_file && -d $first_ssh_agent_file);
   }
-  #none exists for given PID, pull first off list
+
   return $first_ssh_agent_file;
 }
+
